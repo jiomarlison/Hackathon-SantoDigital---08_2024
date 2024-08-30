a929e256d1d18effc95b999abbf5b71b953abcba
jioma <jiomar213@gmail.com>
2024-08-29 22:57:49 -0300

    Incompleto

--- Desafio 0/Exercicio_3.py
+++ Desafio 0/Exercicio_3.py
@@ -1,16 +1,29 @@
 from itertools import combinations
 
-lista = [45, 6, 3, 2, 9]
+lista = [45, 6, 3, 4, 5, 3]
 
 
-def combinacoes(lista: list,
-                max_size: int = 0,
-                min_size: int = 0,
-                distinct_only: bool = False,
-                sort_subsets: bool = False):
-    for valor in lista:
-        print(valor)
-    combinacoes = list(combinations(lista, 2))
+def lista_combinacoes(lista: list,
+                      max_size: int = 1,
+                      min_size: int = -1,
+                      distinct_only: bool = False,
+                      sort_subsets: bool = False
+                      ):
+
+    print("Erro, min_size não pode ser maior que max_size") if min_size > max_size else ...
+    combinacoes = []
+    for tam in range(0, len(lista) + 1):
+        if max_size >= tam >= min_size:
+            for comb in combinations(lista, tam):
+                combinacoes.append(comb)
+
+    if distinct_only is True:
+        combinacoes_sem_duplicatas = []
+        for comb in combinacoes:
+            print(comb)
+
+        combinacoes = combinacoes_sem_duplicatas
+    return combinacoes
 
 
-combinacoes(lista)
+print(lista_combinacoes(lista, 3, 1, True))
