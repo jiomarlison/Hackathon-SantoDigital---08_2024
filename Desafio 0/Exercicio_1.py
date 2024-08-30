ec6e561edf839aac8fe0eec1482c16de26cda927
jioma <jiomar213@gmail.com>
2024-08-29 21:13:41 -0300

    Descrição de Função

--- Desafio 0/Exercicio_1.py
+++ Desafio 0/Exercicio_1.py
@@ -1,4 +1,7 @@
+# Recebe um número e multiplica ele pela string "*"
 def asterisco(n: int):
+    # Faz um List Comprehension multiplicando n por "*" partindo de 1 ate n+1, já que python começa a contagem em 0
+    # tivemos que forçar a comecar do 1 e incrementamos +1 ao valor de n para atingir o valor certo.
     return ['*' * i for i in range(1, n + 1)]
 
 
